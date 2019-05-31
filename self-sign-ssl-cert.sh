5c399e2d62d7fe02ab3aa74e23178aee84f5f1a8
Chinory <chinory@cup.moe>
2019-05-31 12:30:51 +0800

    Update self-sign-ssl-cert.sh

--- self-sign-ssl-cert.sh
+++ self-sign-ssl-cert.sh
@@ -1,3 +1,3 @@
 openssl genrsa -out default.key 2048
-openssl req -new -x509 -days 3650 -key default.key -out default.cer -subj="/C=  /CN=$(curl -s icanhazip.com)"
+openssl req -new -x509 -days 3650 -key default.key -out default.cer -subj="/C=  /CN=$(cat /proc/sys/kernel/random/uuid)"
 #openssl x509 -noout -text -in default.cer
